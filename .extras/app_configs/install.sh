e601e81276a6e00ec7ded876319aa97eaccf8e2c
Jacob Blanco <jayb88@gmail.com>
2021-11-13 18:09:14 +0900

    clean up brew and prettify install

--- .extras/app_configs/install.sh
+++ .extras/app_configs/install.sh
@@ -3,3 +3,5 @@ BASEDIR=$(dirname "$0")
 echo "Setting up Application Configs"
 
 defaults import com.manytricks.Moom $BASEDIR/Moom.plist
+
+defaults write com.runningwithcrayons.Alfred-Preferences syncfolder "~/.alfred"
---
+++ .extras/utils/colors.sh
@@ -0,0 +1,27 @@
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+bg_black=`tput setab 0`
+bg_red=`tput setab 1`
+bg_green=`tput setab 2`
+bg_yellow=`tput setab 3`
+bg_blue=`tput setab 4`
+bg_magenta=`tput setab 5`
+bg_cyan=`tput setab 6`
+bg_white=`tput setab 7`
+
+bold=`tput bold`
+dim=`tput dim`
+
+reset=`tput setaf sgr0`
+
+tick="\u2714"
+cross="\u2716"
+star="\u2605"
+bullet="\u2022"
