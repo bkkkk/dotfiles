41ad168c7c72d6cd819c57af59fdd8426a2428f6
Jacob Blanco <jayb88@gmail.com>
2026-06-06 10:28:28 +0900

    setup mise

---
+++ setups/mise/mise.sh
@@ -0,0 +1,8 @@
+# Detect if running directly vs sourced
+if [[ "$ZSH_EVAL_CONTEXT" == "toplevel" ]]; then
+  DOTPATH="${HOME}/dotfiles"
+fi
+
+mkdir -p "${HOME}/.config/mise"
+ln -sf ${DOTPATH}/mise/mise.toml ${HOME}/.config/mise/config.toml
+mise install 
