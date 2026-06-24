20a3c0c5115ec2445f0ede1ed4139366b71c1900
Jacob Blanco <jayb88@gmail.com>
2026-06-24 09:16:29 +0900

    cp instead of move op cnfig due to security restriction

--- setups/op/op.sh
+++ setups/op/op.sh
@@ -4,5 +4,5 @@ TARGETPATH=${HOME}/.config/op/
 
 log_info "Setting up op configurations"
 mkdir -p ${TARGETPATH}
-ln -sf ${THISPATH}/config ${HOME}/.config/op/config
+cp ${THISPATH}/config ${HOME}/.config/op/config
 log_success "Finished setting up op configurations"
