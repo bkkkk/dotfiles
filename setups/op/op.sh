8151dd6982eb2ca0542055525a8e7c82d2f3368d
Jacob Blanco <jayb88@gmail.com>
2026-06-24 09:16:59 +0900

    Copy instead

--- setups/op/op.sh
+++ setups/op/op.sh
@@ -4,5 +4,7 @@ TARGETPATH=${HOME}/.config/op/
 
 log_info "Setting up op configurations"
 mkdir -p ${TARGETPATH}
+# op doesn't support reading symlinked config files
+# copy instead.
 cp ${THISPATH}/config ${HOME}/.config/op/config
 log_success "Finished setting up op configurations"
